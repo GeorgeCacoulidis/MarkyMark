1e280025bed846b81373133a11dabbe45e274f9c
Andrew <42707892+AndrewMartyn@users.noreply.github.com>
2022-07-27 11:03:42 -0400

    updated backend

--- server/src/nodemailer.js
+++ server/src/nodemailer.js
@@ -2,8 +2,7 @@ const nodemailer = require("nodemailer");
 require("dotenv").config();
 
 let transporter = nodemailer.createTransport({
-    host: 'smtp.titan.email',
-    port: 587,
+    service: "gmail",
     auth: {
         user: process.env.AUTH_EMAIL,
         pass: process.env.AUTH_PASS,
